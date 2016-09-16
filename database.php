241a650d80995aaaa25631eca539497fee51c061
Gautam krishna.R <rgautamkrishna@gmail.com>
2016-09-16 09:45:31 +0000

    added auto deploy to openshift
    added auto deploy to openshift
    
    Fix

--- database.php
+++ database.php
@@ -1,3 +1,3 @@
 <?php
-$result = mysqli_connect("localhost","","","");
+$result = mysqli_connect(getenv("OPENSHIFT_MYSQL_DB_HOST").":".getenv("OPENSHIFT_MYSQL_DB_PORT"),getenv("OPENSHIFT_APP_NAME"),getenv("OPENSHIFT_MYSQL_DB_USERNAME"),getenv("OPENSHIFT_MYSQL_DB_PASSWORD"));
 ?>
