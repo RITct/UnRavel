db31f4faeea5be257ae5f0be718e5d8f4c106122
Gautam krishna.R <rgautamkrishna@gmail.com>
2016-09-18 12:33:28 +0000

    deploy

--- database.php
+++ database.php
@@ -1,8 +1,9 @@
 <?php
-$result = mysqli_connect(getenv("OPENSHIFT_MYSQL_DB_HOST").":".getenv("OPENSHIFT_MYSQL_DB_PORT"),getenv("OPENSHIFT_MYSQL_DB_USERNAME"),getenv("OPENSHIFT_MYSQL_DB_PASSWORD"),getenv("OPENSHIFT_APP_NAME"));
+$result = mysqli_connect(getenv("OPENSHIFT_MYSQL_DB_HOST").":".getenv("OPENSHIFT_MYSQL_DB_PORT"),getenv("OPENSHIFT_MYSQL_DB_USERNAME"),getenv("OPENSHIFT_MYSQL_DB_PASSWORD"),getenv("OPENSHIFT_APP_NrjAME"));
 if(mysqli_connect_error())
 {
-  echo "Database connection error..."
+  
+  echo "Database connection error...";
   exit();
 }
 ?>
