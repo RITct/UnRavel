f442b0bab80462a890683ec9509f540b454e552a
Gautam krishna.R <rgautamkrishna@gmail.com>
2016-09-18 17:07:13 +0530

    Update database.php

--- database.php
+++ database.php
@@ -1,3 +1,8 @@
 <?php
-$result = mysqli_connect(getenv("OPENSHIFT_MYSQL_DB_HOST").":".getenv("OPENSHIFT_MYSQL_DB_PORT"),getenv("OPENSHIFT_APP_NAME"),getenv("OPENSHIFT_MYSQL_DB_USERNAME"),getenv("OPENSHIFT_MYSQL_DB_PASSWORD"));
+$result = mysqli_connect(getenv("OPENSHIFT_MYSQL_DB_HOST").":".getenv("OPENSHIFT_MYSQL_DB_PORT"),getenv("OPENSHIFT_APP_NAME"),getenv("OPENSHIFT_MYSQL_DB_PASSWORD"),getenv("OPENSHIFT_MYSQL_DB_USERNAME"));
+if(mysqli_connect_error())
+{
+  echo "Database connection error..."
+  exit();
+}
 ?>
