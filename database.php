d920ef01301c7efe00607d6da901bb9db6a3cb04
JOBIN PHILIP ABRAHAM <jpa895@gmail.com>
2016-09-18 16:08:07 +0430

    Update index.php
    Update database.php
    
    added

--- database.php
+++ database.php
@@ -1,5 +1,5 @@
 <?php
-$result = mysqli_connect(getenv("OPENSHIFT_MYSQL_DB_HOST").":".getenv("OPENSHIFT_MYSQL_DB_PORT"),getenv("OPENSHIFT_APP_NAME"),getenv("OPENSHIFT_MYSQL_DB_PASSWORD"),getenv("OPENSHIFT_MYSQL_DB_USERNAME"));
+$result = mysqli_connect(getenv("OPENSHIFT_MYSQL_DB_HOST").":".getenv("OPENSHIFT_MYSQL_DB_PORT"),getenv("OPENSHIFT_MYSQL_DB_USERNAME"),getenv("OPENSHIFT_MYSQL_DB_PASSWORD"),getenv("OPENSHIFT_APP_NAME"));
 if(mysqli_connect_error())
 {
   echo "Database connection error..."
--- index.php
+++ index.php
@@ -2,7 +2,6 @@
 session_start();
 require_once("database.php");
 global $result;
-header("Location:mkdb.php");
 echo "<!DOCTYPE html>
 <html>
 <head>
